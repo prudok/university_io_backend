41f833e30bad9449844190477f996c8db04aca63
Roman Prudchenko <romanprudchen@gmail.com>
2023-10-10 20:59:35 +0300

    refactor: added naming fixes

--- lib/data/tables/types_of_activities.dart
+++ lib/data/tables/types_of_activities.dart
@@ -1,6 +1,5 @@
 import 'package:drift/drift.dart';
 import 'package:university_io_backend/data/tables/tables.dart';
-import 'package:university_io_backend/data/tables/volunteering.dart';
 
 class TypesOfActivities extends Table {
   IntColumn get studentId => integer().references(Students, #id)();
