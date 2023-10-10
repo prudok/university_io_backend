86f8fc141878cc1a47d05788f0bfdbeee71251a5
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2023-10-10 20:42:44 +0300

    ficha

--- lib/data/tables/types_of_actities.dart
+++ lib/data/tables/types_of_actities.dart
@@ -1,10 +1,10 @@
 import 'package:drift/drift.dart';
-import 'package:university_io_backend/tables/students.dart';
-import 'package:university_io_backend/tables/volunteering.dart';
+import 'package:university_io_backend/data/tables/tables.dart';
+import 'package:university_io_backend/data/tables/volunteering.dart';
 
 class TypesOfActivities extends Table {
   IntColumn get studentId => integer().references(Students, #id)();
   IntColumn get groupId => integer().references(Groups, #id)();
   IntColumn get volunteeringId => integer().references(Volunteering, #id)();
-  IntColumn get torchId => integer().references(Tourch, #id)();
+  IntColumn get torchId => integer().references(Torches, #id)();
 }
