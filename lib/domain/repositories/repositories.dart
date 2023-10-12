6d7021d361cd63d9346196efba779bfd58ce0f03
Roman Prudchenko <romanprudchen@gmail.com>
2023-10-13 00:34:06 +0300

    feat: added exporting file

---
+++ lib/domain/repositories/repositories.dart
@@ -0,0 +1,26 @@
+//GENERATED BARREL FILE
+export 'accuracy_table_repository.dart';
+export 'alliance_repository.dart';
+export 'book_repository.dart';
+export 'classroom_repository.dart';
+export 'department_repository.dart';
+export 'department_teacher_link_repository.dart';
+export 'event_repository.dart';
+export 'exam_repository.dart';
+export 'group_repository.dart';
+export 'gto_repository.dart';
+export 'library_repository.dart';
+export 'mark_repository.dart';
+export 'news_repository.dart';
+export 'project_repository.dart';
+export 'schedule_repository.dart';
+export 'sport_activity_repository.dart';
+export 'sport_team_repository.dart';
+export 'student_repository.dart';
+export 'subject_repository.dart';
+export 'syllabus_repository.dart';
+export 'teacher_repository.dart';
+export 'torch_repository.dart';
+export 'type_of_activity_repository.dart';
+export 'university_dormitory_repository.dart';
+export 'volunteering_repository.dart';
